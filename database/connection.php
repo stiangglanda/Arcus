061c8fdce9cff70260c8215e6f79968d427fcd9c
Tim <timhofmann01@gmail.com>
2022-03-04 12:23:08 +0100

    updated credentials

--- database/connection.php
+++ database/connection.php
@@ -1,9 +1,9 @@
 <?php
     function OpenCon() {
         $dbhost = "localhost";
-        $dbuser = "root";
-        $dbpass = "1234";
-        $db = "example";
+        $dbuser = "id18558812_arcus";
+        $dbpass = "vrbLW!V?o0-H/(Fk";
+        $db = "id18558812_arcusdb";
         $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
         return $conn;
     }
