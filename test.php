426e32b2cc46f20e27185c6f57cd3d380c41211f
Tim <timhofmann01@gmail.com>
2022-03-11 11:47:19 +0100

    added test file for experimenting

---
+++ test.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title> How to call PHP function on the click of a Button ? </title>
+</head>
+
+<body style="text-align:center;">
+    <?php
+
+    // get post records
+    $firstName = $_GET['firstName'];
+    $lastName = $_GET['lastName'];
+    $nickName = $_GET['username'];
+    $password = $_GET['password'];
+
+    echo "test";
+    echo "$firstName";
+    // echo "This is Button1 that is selected";
+    // echo "This is Button1 that is selected";
+    ?>
+    </head>
+
+</html>
