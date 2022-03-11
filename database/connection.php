a103e2d3e3214f828258b7cf558ca20dd40d9de9
Tim <timhofmann01@gmail.com>
2022-03-11 08:31:04 +0100

    neues connection system

--- database/connection.php
+++ database/connection.php
@@ -1,15 +1,19 @@
 <?php
-$servername = "localhost";
-$username = "id18558812_arcus";
-$password = "vrbLW!V?o0-H/(Fk";
-$database = "id18558812_arcusdb";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    } catch(PDOException $e) {    
-    echo "Connection failed: " . $e->getMessage();
-    }
-?>
+require_once 'config.php';
+
+function connect($host, $database, $user, $password)
+{
+	$dsn = "mysql:host=$host;dbname=$database;charset=UTF8";
+
+	try {
+		$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+		return new PDO($dsn, $user, $password, $options);
+	} catch (PDOException $e) {
+        echo $e->getMessage();
+		die($e->getMessage());
+	}
+}
+
+return connect($host, $db, $user, $password);
