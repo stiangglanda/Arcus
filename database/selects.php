8c81556ac40a6b1b79d8d986bacdf74312ea988f
Tim <timhofmann01@gmail.com>
2022-03-18 10:26:22 +0100

    started adding more tables to json

--- database/selects.php
+++ database/selects.php
@@ -1,33 +1,72 @@
 <?php
-try {
-    $pdo = require '../database/connect.php';
-    $sql = "SELECT userId, firstName, lastName FROM user";
-    $array = array();
-    $count = 0;
+function getUser()
+{
+    try {
+        $pdo = require '../database/connect.php';
+        $sql = "SELECT userId, firstName, lastName, nickName, password, guest FROM user"; // call getUser;
+        $array = array();
+        $count = 0;
 
-    // save result rows to array
-    foreach ($pdo->query($sql) as $row) {
-        $array += array(
-            $count => array(
-                "userId" => $row['userId'], // int
-                "firstName" => $row['firstName'], // string
-                "lastName" => $row['lastName'], // string
-                "nickName" => $row['nickName'], // string
-                "password" => $row['password'], // string
-                "guest" => $row['guest'] // bool (0 = no guest, 1 = guest)
-            )
-        );
+        // save result rows to array
+        foreach ($pdo->query($sql) as $row) {
+            $array += array(
+                $count => array(
+                    "userId" => $row['userId'], // int
+                    "firstName" => $row['firstName'], // string
+                    "lastName" => $row['lastName'], // string
+                    "nickName" => $row['nickName'], // string
+                    "password" => $row['password'], // string
+                    "guest" => $row['guest'] // bool (0 = no guest, 1 = guest)
+                )
+            );
 
-        $count++;
+            $count++;
+        }
+
+        // encode array to json
+        $json = json_encode(array('user' => $array));
+
+        //write json to file
+        if (!file_put_contents("data.json", $json)) {
+            echo "Oops! Error creating json file...";
+        }
+    } catch (\Throwable $e) {
+        die($e->getMessage());
     }
+}
 
-    // encode array to json
-    $json = json_encode(array('data' => $array));
+function getScore()
+{
+    try {
+        $pdo = require '../database/connect.php';
+        $sql = "call getScore();";
+        $array = array();
+        $count = 0;
 
-    //write json to file
-    if (!file_put_contents("data.json", $json)) {
-        echo "Oops! Error creating json file...";
+        // save result rows to array
+        foreach ($pdo->query($sql) as $row) {
+            $array += array(
+                $count => array(
+                    "userId" => $row['userId'], // int
+                    "firstName" => $row['firstName'], // string
+                    "lastName" => $row['lastName'], // string
+                    "nickName" => $row['nickName'], // string
+                    "password" => $row['password'], // string
+                    "guest" => $row['guest'] // bool (0 = no guest, 1 = guest)
+                )
+            );
+
+            $count++;
+        }
+
+        // encode array to json
+        $json = json_encode(array('data' => $array));
+
+        //write json to file
+        if (!file_put_contents("data.json", $json)) {
+            echo "Oops! Error creating json file...";
+        }
+    } catch (\Throwable $e) {
+        die($e->getMessage());
     }
-} catch (\Throwable $e) {
-    die($e->getMessage());
 }
