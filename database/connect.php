bc801f86a4bfbda8c6351a02cde15b3f853ccc9c
Tim <timhofmann01@gmail.com>
2022-03-18 09:56:02 +0100

    save data to json

--- database/connect.php
+++ database/connect.php
@@ -11,9 +11,9 @@ function connect($db_host, $db_database, $db_user, $db_password)
 
 		$pdo = new PDO($dsn, $db_user, $db_password, $options);
 
-		if ($pdo) {
-			// echo "Successfully connected to $db_database";
-		}
+		// if ($pdo) {
+		// 	echo "Successfully connected to $db_database";
+		// }
 
 		return $pdo;
 	} catch (PDOException $e) {
