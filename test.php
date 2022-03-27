a821e1ae8a8cb5958525d87244d6d2f02d429e53
ItsThisTim <timhofmann01@gmail.com>
2022-03-27 14:11:36 +0200

    improved test.php (with css now)

--- test.php
+++ test.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>PHP Test</title>
+    <link rel="stylesheet" href="./test.css">
 </head>
 
 <body>
     <?php
-
     require_once "./classes/db.php";
     require_once "./classes/animal.php";
     require_once "./classes/pevent.php";
@@ -20,31 +20,61 @@
     $db = new Database();
     $utils = new Utils();
 
-
     $animal = new Animal();
+    $user = new User();
     $event = new PEvent();
     $parcour = new Parcour();
     $score = new Score();
 
-    // todo: define id? or let auto_increment auto select it but lose access to id
-    $user = new User($db::nextId("user"), "Lena", "Wurmsdobler", "Leni", "1234", 0);
+    echo "<hr>Objects can exists without necessarily existing in the database.<br><br>";
+    $jan = new User($utils::nextId("user"), "Jan", "Hofmann", "jan", "1234", 1);
+    $tim = $user->getUserByNickName("ThisTim");
 
-    try {
-        $user->insert();
-    } catch (PDOException $e) {
-        echo $utils->getPdoErr($e) . "<hr>";
-    }
+    echo $tim ? "Tim Object exists<br>" : "Tim Object does not exist<br>";
+    echo $jan ? "Jan Object exists<br><br>" : "Jan Object does not exist<br><br>";
+    echo $tim->exists() ? "Tim exists in the db.<br>" : "Tim does not exist in the db.<br>";
+    echo $jan->exists() ? "Jan exists in the db.<br>" : " Jan does not exist in the db.<br>";
 
-    echo "<hr>";
-    listUsers($user->getUsers());
-    echo "<hr>";
+    echo "<br><hr>If an object only has null values then only static methods can be used.<br><br>";
+    listUsers($user::getUsers());
+    echo '<br>Insert new user with \'$user->insert();\' (only actually inserts if possible without errors)<br>';
 
     function listUsers($users)
     {
-        foreach ($users as $userrow) {
-            echo $userrow['userId'] . " " . $userrow['firstName'] . " " . $userrow['lastName'] . " " . $userrow['nickName'] . " " . $userrow['password'] . " " . $userrow['guest'] . "<br>";
-        }
+    ?>
+        <table>
+            <thead>
+                <tr>
+                    <th>userId</th>
+                    <th>firstName</th>
+                    <th>lastName</th>
+                    <th>nickName</th>
+                    <th>password</th>
+                    <th>guest</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($users as $userObj) {
+                ?>
+                    <tr>
+                        <td><?= $userObj->userId ?></td>
+                        <td><?= $userObj->firstName ?></td>
+                        <td><?= $userObj->lastName ?></td>
+                        <td><?= $userObj->nickName ?></td>
+                        <td><?= $userObj->password ?></td>
+                        <td><?= $userObj->guest ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    <?php
     }
+
+    echo "<br><hr>Utils class for small utlities that don't fit another class.<br><br>";
+    echo "Next userId: " . $utils::nextId("user") . "<br>";
     ?>
 </body>
 
