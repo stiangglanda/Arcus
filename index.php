51acb2eda09727730cdfe17b34746a44c03b2c4e
Tim <timhofmann01@gmail.com>
2022-04-01 09:15:43 +0200

    changed form action & button

--- index.php
+++ index.php
@@ -49,7 +49,7 @@
                                         <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
                                         <p class="text-center small">Enter your username & password to login</p>
                                     </div>
-                                    <form class="row g-3 needs-validation" action="./forms/login.php" method="post" novalidate>
+                                    <form class="row g-3 needs-validation" action="./index.php" method="post" novalidate>
                                         <div class="col-12">
                                             <label for="yourUsername" class="form-label">Username</label>
                                             <div class="input-group has-validation">
@@ -64,7 +64,7 @@
                                             <div class="invalid-feedback">Please enter your password!</div>
                                         </div>
                                         <div class="col-12">
-                                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                                            <button class="btn btn-primary w-100" type="submit" name="submit">Login</button>
                                             <a href="html/dashboard.php">Dashboard</a>
                                         </div>
                                         <div class="col-12">
