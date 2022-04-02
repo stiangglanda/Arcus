059c5bdac1e9225ceaee1874744c9d0413299065
Tim <timhofmann01@gmail.com>
2022-04-02 15:49:29 +0200

    fixed path

--- html/signin.php
+++ html/signin.php
@@ -21,7 +21,7 @@
                                     <h5 class="card-title text-center pb-0 fs-4">Sign in to your account</h5>
                                     <p class="text-center small">Enter your username & password to sign in</p>
                                 </div>
-                                <form class="row g-3 needs-validation" action="./forms/signin.php" method="post"
+                                <form class="row g-3 needs-validation" action="../forms/signin.php" method="post"
                                     novalidate>
                                     <div class="col-12">
                                         <label for="yourUsername" class="form-label">Username</label>
