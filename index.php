8b41ee98e40ea9cd315f21e809ab130df839e67f
Tim <timhofmann01@gmail.com>
2022-04-02 16:01:48 +0200

    Revert "refactoring and session tests"
    This reverts commit d4fc605c17f365d592de0beba9887d1bfcc40fe5.

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
+session_start();
+
 require_once "./classes/db.php";
 require_once "./classes/utils.php";
 require_once "./classes/user.php";
-require_once "./classes/parcour.php";
-require_once "./classes/pevent.php";
-require_once "./classes/animal.php";
-require_once "./classes/score.php";
 
-session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -16,24 +14,24 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Login</title>
+    <title>Sign In</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
     <!-- Favicons -->
-    <link href="assets/img/arrows.png" rel="icon">
+    <link href="./assets/img/arrows.png" rel="icon">
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <!-- Vendor CSS Files -->
-    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
-    <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-    <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+    <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="./assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="./assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="./assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="./assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="./assets/vendor/simple-datatables/style.css" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="./assets/css/style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -42,35 +40,32 @@ session_start();
 if (isset($_GET['page'])) {
     switch ($_GET['page']) {
         case 'Register':
-            include 'html/Register.php';
+            include './html/Register.php';
             break;
         case 'addPlayer':
-            include 'html/addPlayer.php';
+            include './html/addPlayer.php';
             break;
         case 'addParcour':
-            include 'html/addParcour.php';
+            include './html/addParcour.php';
             break;
         case 'Statistics':
-            include 'html/Statistics.php';
+            include './html/Statistics.php';
             break;
         case 'dashboard':
-            include 'html/dashboard.php';
+            include './html/dashboard.php';
             break;
         case 'Settings':
-            include 'html/Settings.php';
+            include './html/Settings.php';
             break;
         case 'Game':
-            include 'html/Game.php';
-            break;
-        case 'LogIn':
-            include 'html/LogIn.php';
+            include './html/Game.php';
             break;
         default:
-            include 'html/start.php';
+            include './html/start.php';
     }
 }
 else {
-    include 'html/start.php';
+    include './html/start.php';
 }
 ?>
        
