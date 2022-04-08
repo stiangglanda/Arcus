192431081a5fd886fa416f98b044e572c1b43080
Tim <timhofmann01@gmail.com>
2022-04-08 09:45:45 +0200

    fixed expiry of information schema

--- classes/utils.php
+++ classes/utils.php
@@ -5,6 +5,9 @@ class Utils extends Database
     public static function nextId($table)
     {
         $db = new Database();
+        $stmt = $db->pdo->prepare("SET GLOBAL information_schema_stats_expiry = 0");
+        $stmt->execute();
+        
         $stmt = $db->pdo->prepare("SELECT AUTO_INCREMENT FROM information_schema.tables WHERE lower(table_name) = lower(?) AND table_schema = DATABASE()");
         $stmt->execute([$table]);
 
